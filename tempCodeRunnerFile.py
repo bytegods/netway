014fcfcf9fca2cc98a4365e12dad87570aef42a5
Thesaderror <88781785+thesaderror@users.noreply.github.com>
2022-11-09 19:15:18 +0400

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-
-data = {
-  "content": "Testing post method..."
-}
-
-url = "https://discord.com/api/webhooks/1039783739099709470/p-4OTStZeKRd-fzXUQwXmUqMQAhPYIAiDCYj3NYQgpPL7a9SDB8t1nChkYnwiWsmgKxO"
-nw = netway.post(url,payload=data)
-print(nw.text)
